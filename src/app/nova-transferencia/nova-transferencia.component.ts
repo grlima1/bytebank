9f51e54ee186870f6bcf344e316f4fd6f552d6c7
Gabriel Lima <gabriellima_black@hotmail.com>
2021-12-27 09:19:25 -0300

    Commit inicial

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  @Output() valoresComErro = new EventEmitter<string>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência!');
+
+    if(this.ehValido()){
+      const valorEmitir = { valor: this.valor, destino: this.destino };
+      this.aoTransferir.emit(valorEmitir);
+    }
+
+    this.limparCampos();
+  }
+
+  private ehValido(){
+    const valido = this.valor > 0;
+    if (!valido){
+      this.valoresComErro.emit('Informe um valor válido');
+    }
+    return valido;
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
